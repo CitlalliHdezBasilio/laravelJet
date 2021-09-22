70b2485eb4c26397aa3150d01817a108b1ba8cdf
CitlalliHdezBasilio <91081487+CitlalliHdezBasilio@users.noreply.github.com>
2021-09-22 14:17:39 -0500

    Update jetstream.php

--- config/jetstream.php
+++ config/jetstream.php
@@ -41,13 +41,13 @@ return [
     |
     */
 
-/*     'features' => [
+     'features' => [
         // Features::termsAndPrivacyPolicy(),
          Features::profilePhotos(),
         //Features::api(),
         Features::teams(['invitations' => true]),
         Features::accountDeletion(),
-    ], */
+    ], 
 
     /*
     |--------------------------------------------------------------------------
