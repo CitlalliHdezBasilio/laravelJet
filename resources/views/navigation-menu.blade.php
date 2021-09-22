0cbb8a2961d365b2b4dccc6c93f533418ffa90ab
CitlalliHdezBasilio <citlallihernandezbasilio@gmail.com>
2021-09-22 13:08:39 -0500

    agregando candidatos al menu

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -15,6 +15,9 @@
                     <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-jet-nav-link>
+                    <x-jet-nav-link href="{{ route('nuevo') }}" :active="request()->routeIs('nuevo')">
+                        {{ __('Candidatos') }}
+                    </x-jet-nav-link>
                 </div>
             </div>
 
@@ -141,6 +144,9 @@
             <x-jet-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-jet-responsive-nav-link>
+            <x-jet-nav-link href="{{ route('nuevo') }}" :active="request()->routeIs('nuevo')">
+                        {{ __('Candidatos') }}
+                    </x-jet-nav-link>
         </div>
 
         <!-- Responsive Settings Options -->
