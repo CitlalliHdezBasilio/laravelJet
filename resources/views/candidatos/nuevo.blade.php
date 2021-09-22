133a559bb38fbc86a0272c920b5172c3876b53af
CitlalliHdezBasilio <citlallihernandezbasilio@gmail.com>
2021-09-22 16:07:50 -0500

    vista nuevo candidato

--- resources/views/candidatos/nuevo.blade.php
+++ resources/views/candidatos/nuevo.blade.php
@@ -5,11 +5,40 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
-            </div>
+  <div class="py-12">
+     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+ <!--                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg"> -->
+                
+            
+  <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+    <div class="flex -mx-2">
+        <div class="w-1/3 bg-gray-400 h-12 px-2">
+            <label class="block text-gray-700 text-sm font-bold mb-2" for="nombre">
+                Nombre
+            </label>
+            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text">
         </div>
+        <div class="w-1/3 bg-gray-400 h-12 px-2">
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="nombre">
+                Apellido paterno
+            </label>
+            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text">
+        </div>
+        <div class="w-1/3 bg-gray-400 h-12"></div>
     </div>
+
+    <div class="flex items-center justify-between">
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+        Sign In
+      </button>
+      <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
+        Forgot Password?
+      </a>
+    </div>
+  </form>
+
+
+            <!-- </div>-->
+        </div>
+    </div> 
 </x-app-layout>
--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -142,11 +142,11 @@
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-jet-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+                {{ __('Inicio') }}
             </x-jet-responsive-nav-link>
             <x-jet-nav-link href="{{ route('nuevo') }}" :active="request()->routeIs('nuevo')">
-                        {{ __('Candidatos') }}
-                    </x-jet-nav-link>
+                {{ __('Candidatos') }}
+            </x-jet-nav-link>
         </div>
 
         <!-- Responsive Settings Options -->
